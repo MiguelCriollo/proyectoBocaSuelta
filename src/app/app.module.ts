6c754260329a64e06fe692a1c35d3fba511eafb5
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-05-26 17:40:18 -0500

    Menu 1 Completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Menu2Component } from './menu2/menu2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Menu2Component
   ],
   imports: [
     BrowserModule,
